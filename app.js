45dbfa70a9aa6f3495cd5259ededff92a7430d4d
Hasan Taha Künkül <tahaknkl2323@gmail.com>
2024-01-26 18:25:20 +0300

    Yaptığınız değişiklikler hakkında açıklama

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Merhaba, Docker!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server ${PORT} portunda çalışıyor...`);
+});
